284168ac87b0243ded3b60d0d9ef846275c71dbb
Oksana <starday2009@mail.ru>
2017-04-24 14:09:19 +0300

    Jira enter

---
+++ src/main/java/Selenium2Example.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Selenium2Example {
+    public static void main(String[] args) throws InterruptedException {
+        // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface,
+        // not the ismplementation.
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+        WebDriver driver = new FirefoxDriver(capabilities);
+
+        // And now use this to visit Google
+        driver.get("http://soft.it-hillel.com.ua:8080/browse/QAAUT-60");
+
+        //login
+        WebElement loginField = driver.findElement(By.xpath(".//*[@id='login-form-username']"));
+        loginField.clear();
+        loginField.sendKeys("oksana.gorbachenko.2009");
+
+        WebElement passwordField =  driver.findElement(By.xpath(".//*[@id='login-form-password']"));
+        passwordField.clear();
+        passwordField.sendKeys("123456qwerty");
+
+        WebElement loginButton =  driver.findElement(By.xpath(".//*[@id='login-form-submit']"));
+        loginButton.click();
+
+        //Creating sub-task
+        WebElement addSubButton =  driver.findElement(By.xpath(".//*[@id='stqc_show']"));
+        addSubButton.click();
+
+        WebElement summaryField =  driver.findElement(By.xpath(".//*[@id='summary']"));
+        summaryField.clear();
+        summaryField.sendKeys("Oksana`s Sub-Task");
+
+
+        WebElement assignButton =  driver.findElement(By.xpath(" .//*[@id='assign-to-me-trigger']"));
+        assignButton.click();
+
+        WebElement submitButton =  driver.findElement(By.xpath(".//*[@id='create-issue-submit']"));
+        submitButton.click();
+
+
+
+        //Close the browser
+
+        //driver.quit();
+    }
+}
